33718e8b45fef9c4e1df9856006dfc0c615519e2
ayushghegde <ayushghegde@gmail.com>
2026-09-02 21:43:06 +0530

    v14R: add Magic branch-mouth source

---
+++ experiments/v14R_polarity_guided_choice_cell/physical/choice5_m2.cmd
@@ -0,0 +1,35 @@
+:load v14r_choice5_m2
+:box 0 0 14 126
+:paint m2
+:box 14 0 44 14
+:paint m2
+:box 14 28 44 42
+:paint m2
+:box 14 56 44 70
+:paint m2
+:box 14 84 44 98
+:paint m2
+:box 14 112 44 126
+:paint m2
+:box 58 0 88 14
+:paint m2
+:label B0 center m2
+:box 58 28 88 42
+:paint m2
+:label B1 center m2
+:box 58 56 88 70
+:paint m2
+:label B2 center m2
+:box 58 84 88 98
+:paint m2
+:label B3 center m2
+:box 58 112 88 126
+:paint m2
+:label B4 center m2
+:box 0 0 14 126
+:label CHOICE center m2
+:drc check
+:drc count total
+:save
+:extract all
+:quit
